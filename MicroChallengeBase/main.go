c82ac34bcad5c7f41e420b79f2cc87e45bd60fd6
ShacharBartal <shacharbartal@gmail.com>
2020-03-26 20:51:12 +0200

    Family name works, still not modulirize

--- MicroChallengeBase/main.go
+++ MicroChallengeBase/main.go
@@ -1,9 +1,57 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 )
+
+type person struct{
+	FirstName string
+	FamilyName string
+}
+
 
 func main(){
 	fmt.Println("Hello from base")
+
+	p1 := person{}
+
+	client := &http.Client{} // define MicroChallengeBase as a client
+
+	//will get familyName with following command:
+	familyNameMicroServiceResponse , err := infrastructure(client, "http://127.0.0.1:8081/familyName")
+
+	if err != nil { // we didnt success the command above ^
+		fmt.Printf("infrastructure error: %v\n", err)
+	} else { // successed
+		fmt.Printf("raw response: %s\n", string(familyNameMicroServiceResponse))
+		err := json.Unmarshal(familyNameMicroServiceResponse, &p1) // convert familyNameMicroServiceResponse to p1
+		if err != nil {
+			fmt.Printf("unmarshal error: %v\n", err)
+		} else {
+			fmt.Printf("Family name response: %+v\n", p1)
+		}
+
+	}
+
+}
+
+// create http request
+// send http request
+// convert byte[] response to string
+func infrastructure(client *http.Client, url string) ([]byte, error) {
+	req, err := http.NewRequest("GET", url, nil)
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("client req error: " + err.Error())
+	} else {
+		responseBody, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return nil, fmt.Errorf("body read error: " + err.Error())
+		} else {
+			return responseBody, err
+		}
+	}
 }
--- MicroChallengeFamilyName/main.go
+++ MicroChallengeFamilyName/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+//declare the function we are getting from [address]/familyName
+
+func getFamilyName(context *gin.Context){
+	familyName := fmt.Sprintf(("Bartal"))
+	fmt.Println("[Debug] Returning family name: %s", familyName)
+
+	//We will return the string as JSON and send an http ok message. H will create the JSON
+	context.JSON(http.StatusOK, gin.H{"FamilyName" : familyName})
+}
 
 func main(){
 	fmt.Printf("Hello from family name ")
+	//creating the remote:
+	router := gin.Default()
+
+	//declare - if we go to address/FamilyName - we will go to getFamilyName() function
+	router.GET("/familyName", getFamilyName)
+
+	//running our router on 8081 port
+	router.Run(":8081")
 }
+
