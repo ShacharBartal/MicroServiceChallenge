d291ef96736bc876b81bb63417b3297e5f3fcb36
ShacharBartal <shacharbartal@gmail.com>
2020-03-30 10:40:46 +0300

    add commands to firstNameMain

--- MicroChallengeFirstName/main.go
+++ MicroChallengeFirstName/main.go
@@ -3,19 +3,21 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin" // gin is framework for RESTapi
+	"net/http"
 )
 
-var firstName = "Gal"
-
-func getFirstName(context *gin.Context){ // must use context for this framework
-
+func getFirstName(context *gin.Context) { // must use context for this framework
+	firstName := fmt.Sprintf(("Shachar"))
+	fmt.Println("[Debug] Returning first name: %s", firstName)
+	//We will return the string as JSON and send an http ok message. H will create the JSON
+	context.JSON(http.StatusOK, gin.H{"FirstName": firstName})
 }
 
 func main(){
 
-	fmt.Printf("Hello from first name")
-	router := gin.Default()
-	router.GET("/getFirstName", getFirstName)
-	router.Run(":8014")
+	fmt.Printf("Hello from first name") // print for debug
+	router := gin.Default() // create a router
+	router.GET("/getFirstName", getFirstName) // create a get method
+	router.Run(":8014") // run the router
 
 }
